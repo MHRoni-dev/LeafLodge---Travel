a1e5de9845c41e8f65d0056e0b19a29690d73e1a
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-10-28 20:26:52 +0600

    feat(header): add animation interactivity

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,14 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge"
+import resolveConfig from 'tailwindcss/resolveConfig';
+import tailwindConfig from '../../tailwind.config';
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+
+export function getTailwindColors () {
+  const fullConfig = resolveConfig(tailwindConfig);
+  return fullConfig.theme.colors
+} 
