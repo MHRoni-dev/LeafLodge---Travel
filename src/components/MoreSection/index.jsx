a9f0a6622ed310f0d05db31a1da7cec1cfd2e520
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-10-29 14:47:51 +0600

    refactor(section): add responsiveness in more section

--- src/components/MoreSection/index.jsx
+++ src/components/MoreSection/index.jsx
@@ -7,12 +7,12 @@ export default function MoreSection() {
     const text03 = 'Lörem ipsum stafettläkare deciledes plal repärad autosperat. Prenat midevis än prolingar åkafusam oheten. Antilig miböskap deck alltså pont. Gygen suder, lere men vadid.'
 
   return (
-    <div className='mt-20 px-28 min-h-screen'>
+    <div className='mt-20 px-8  min-h-screen container mx-auto lg:px-12'>
         <SectionTitle title="MORE WAYS TO SAVE ON YOUR STAY" className="mb-20"/>
-        <div className='grid grid-cols-3 gap-20'>
-            <div className='flex flex-col gap-8'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20'>
+            <div className='flex flex-col gap-4 lg:gap-8'>
                 <img src="/moreSectionOne.png" alt="more section one image" />
-                <h6 className='text-xl font-semibold h-12'>Pay with Points & Cash</h6>
+                <h6 className='text-lg lg:text-xl font-semibold  lg:h-12'>Pay with Points & Cash</h6>
                 <div>
                     {text01.split('').map((char, index) => (
                         <motion.span
@@ -27,9 +27,9 @@ export default function MoreSection() {
                     ))}
                 </div>
             </div>
-            <div className='flex flex-col gap-8'>
+            <div className='flex flex-col gap-4 lg:gap-8'>
                 <img src="/moreSectionTwo.png" alt="more section one image" />
-                <h6 className='text-xl font-semibold  h-12'>Enjoy endless possibilities with Points Purchase</h6>
+                <h6 className='text-lg lg:text-xl font-semibold  lg:h-12'>Enjoy endless possibilities with Points Purchase</h6>
                 <div>
                 {text02.split('').map((char, index) => (
                         <motion.span
@@ -44,9 +44,9 @@ export default function MoreSection() {
                     ))}
                 </div>
             </div>
-            <div className='flex flex-col gap-8'>
+            <div className='flex flex-col gap-4 lg:gap-8'>
                 <img src="/moreSectionThree.png" alt="more section one image" />
-                <h6 className='text-xl font-semibold  h-12'>Moments to remember</h6>
+                <h6 className='text-lg lg:text-xl font-semibold '>Moments to remember</h6>
                 <div>
                 {text03.split('').map((char, index) => (
                         <motion.span
